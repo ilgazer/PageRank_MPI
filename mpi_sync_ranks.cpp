0549b2a3a8c8da90bbac2c944a1b3cd0c409b41d
Ilgaz Er <er.ilgaz@gmail.com>
2023-01-01 22:37:50 +0300

    Implemented actual score calculation

--- mpi_sync_ranks.cpp
+++ mpi_sync_ranks.cpp
@@ -4,17 +4,60 @@
 #include <unordered_map>
 #include <vector>
 #include <iostream>
+#include <array>
 
 typedef long node_id;
 typedef double rank;
 typedef std::pair<node_id, rank> node_pair;
 
-std::unordered_map<node_id, rank> *curr_ranks;
-std::unordered_map<node_id, rank> *next_ranks;
+template <typename S>
+std::ostream &operator<<(std::ostream &os,
+                         const std::vector<S> &vector)
+{
+  // Printing all the elements
+  // using <<
+  os << "[";
+  for (S element : vector)
+  {
+    os << element << ", ";
+  }
+  os << "]";
+  return os;
+}
+
+template <typename S, size_t N>
+std::ostream &operator<<(std::ostream &os,
+                         const std::array<S, N> &vector)
+{
+  // Printing all the elements
+  // using <<
+  os << "[";
+  for (S element : vector)
+  {
+    os << element << ", ";
+  }
+  os << "]";
+  return os;
+}
+
+class Node
+{
+public:
+  node_id id;
+  int num_outgoing_edges;
+  std::vector<node_id> incoming_edges;
+};
+
+static const double alpha = 0.8;
+
+static std::vector<Node *> owned_nodes;
+
+static std::unordered_map<node_id, rank> *curr_ranks;
+static std::unordered_map<node_id, rank> *next_ranks;
 // For every pid, stores the list of local nodes that thread is interested in.
-std::vector<std::vector<node_id> > out_facing_nodes_by_pid;
+static std::vector<std::vector<node_id>> out_facing_nodes_by_pid;
 // Number of nodes we expect to receive from this pid
-std::vector<size_t> num_incoming_nodes_by_pid;
+static std::vector<size_t> num_incoming_nodes_by_pid;
 
 int mypid;
 int numprocs;
@@ -47,9 +90,8 @@ void recv_ranks_for(int pid)
   auto rank_updates = new node_pair[num_incoming_nodes];
   MPI_Status stat;
   MPI_Recv(rank_updates, num_incoming_nodes * sizeof(node_pair), MPI_CHAR, pid, 1, MPI_COMM_WORLD, &stat);
-  
+
   (*next_ranks).insert(rank_updates, rank_updates + num_incoming_nodes);
-  
 }
 
 void sync_ranks()
@@ -83,7 +125,8 @@ void push_mock_data()
   }
 }
 
-void init_vars(){
+void init_vars()
+{
   num_incoming_nodes_by_pid.reserve(numprocs);
   out_facing_nodes_by_pid.reserve(numprocs);
 
@@ -91,6 +134,109 @@ void init_vars(){
   {
     num_incoming_nodes_by_pid.push_back(0);
     out_facing_nodes_by_pid.push_back(std::vector<node_id>());
+  }
+}
+
+double calculate_ranks()
+{
+  double sigma = 0;
+  for (Node *node : owned_nodes)
+  {
+    double sum = 0;
+    for (node_id inc_edge : node->incoming_edges)
+    {
+      sum += (*curr_ranks)[inc_edge];
+    }
+    rank rank = (sum * alpha + (1 - alpha));
+    sigma += std::abs(node->num_outgoing_edges * (*curr_ranks)[node->id] - rank);
+    (*next_ranks)[node->id] = rank / node->num_outgoing_edges;
+  }
+  return sigma;
+}
+
+int communicate_sigma(double sigma)
+{
+  if (mypid != 0)
+  {
+    MPI_Ssend(&sigma, sizeof(double), MPI_CHAR, 0, 1, MPI_COMM_WORLD);
+    int cont;
+    MPI_Status stat;
+    MPI_Recv(&cont, sizeof(int), MPI_CHAR, 0, 1, MPI_COMM_WORLD, &stat);
+    return cont;
+  }
+  else
+  {
+    double sum_sigmas = sigma;
+    for (int pid = 1; pid < numprocs; pid++)
+    {
+      double recv_sigma;
+      MPI_Status stat;
+      MPI_Recv(&recv_sigma, sizeof(double), MPI_CHAR, pid, 1, MPI_COMM_WORLD, &stat);
+      sum_sigmas += recv_sigma;
+    }
+    int cont = sum_sigmas < 1e-6;
+    for (int pid = 1; pid < numprocs; pid++)
+    {
+      MPI_Ssend(&cont, sizeof(int), MPI_CHAR, pid, 1, MPI_COMM_WORLD);
+    }
+    return cont;
+  }
+}
+
+template <typename T, typename K>
+std::array<node_pair, 5> find_top_five(T *t, K get_pair)
+{
+  std::array<node_pair, 5> maximums;
+  std::fill(maximums.begin(), maximums.end(), std::pair(-1, -1));
+
+  for (auto &&raw : *t)
+  {
+    node_pair pair = get_pair(raw);
+    size_t breaking = -1;
+    for (size_t j = 0; j < 5; j++)
+    {
+      if (maximums[j].second <= item.second)
+      {
+        breaking = j;
+      }
+      else
+      {
+        break;
+      }
+    }
+    if (breaking != -1)
+    {
+      for (size_t j = 0; j < breaking; j++)
+      {
+        maximums[j] = maximums[j + 1];
+      }
+      maximums[breaking] = item;
+    }
+  }
+  return maximums;
+}
+
+void consolidate_top_five()
+{
+  auto local_top_five = find_top_five(
+      &owned_nodes,
+      [](Node & node) -> auto{ return std::pair(node.id, (*curr_ranks)[node.id] * node.num_outgoing_edges); });
+
+  if (mypid != 0)
+  {
+    MPI_Ssend(&local_top_five, sizeof(local_top_five), MPI_CHAR, 0, 1, MPI_COMM_WORLD);
+  }
+  else
+  {
+    node_pair *candidates = new node_pair[5 * numprocs];
+    std::copy(local_top_five.begin(), local_top_five.end(), candidates);
+    for (int pid = 0; pid < numprocs; pid++)
+    {
+      MPI_Status stat;
+      MPI_Recv(candidates + 5 * pid, sizeof(local_top_five), MPI_CHAR, pid, 1, MPI_COMM_WORLD, &stat);
+    }
+    auto global_top_five = find_top_five(candidates, [](auto candidate) -> auto{return candidate});
+    std::cout << global_top_five << "\n";
   }
 }
 
@@ -108,14 +254,22 @@ int main(int argc, char *argv[])
   MPI_Comm_rank(MPI_COMM_WORLD, &mypid);    /* get current process id */
   MPI_Comm_size(MPI_COMM_WORLD, &numprocs); /* get number of processes */
 
-
   curr_ranks = new std::unordered_map<node_id, rank>;
   next_ranks = new std::unordered_map<node_id, rank>;
   init_vars();
   push_mock_data();
-  sync_ranks();
-  
-  for (auto &&item: *next_ranks)
+
+  int cont = 1;
+  while (cont)
+  {
+    sync_ranks();
+    double sigma = calculate_ranks();
+    cont = communicate_sigma(sigma);
+    std::swap(curr_ranks, next_ranks);
+    next_ranks->clear();
+  }
+
+  for (auto &&item : *next_ranks)
   {
     std::cout << item.first << ": " << item.second << "\n";
   }
